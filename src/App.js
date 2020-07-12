f71009a8378e8a307f0d4f54b28582c7d318280d
Majid Darvishnejad <m.darvishnejad@Tseco.net>
2020-07-12 11:50:07 +0430

    Fix styles in header css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import React from 'react';
-import './App.css';
-import Header from './components/header';
-import Footer from './components/footer';
+import React from "react";
+import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Footer/>
+      <Home />
     </div>
   );
 }
--- src/components/header.js
+++ src/components/header.js
@@ -1,61 +1,57 @@
-import React,{Component} from 'react';
-import '../css/header.css';
-import group from '../assets/group.png';
-import product from '../assets/product.png';
-import order from '../assets/order.png';
-import notification from '../assets/notification.png';
-import girl from '../assets/girl.jpg';
+import React, { Component } from "react";
+import "../css/header.css";
+import group from "../assets/group.png";
+import product from "../assets/product.png";
+import order from "../assets/order.png";
+import notification from "../assets/notification.png";
+import girl from "../assets/girl.jpg";
 // import hey from '../assets/hey.png';
 
 class Header extends Component {
-    state = {  }
-    render() { 
-        return ( 
-        
-            <div >
-                <p>
-                    <div id="headerLinks" class="row">
+  state = {};
+  render() {
+    return (
+      <div>
+        <p>
+          <div id="headerLinks" class="row">
+            {/* <img src={hey} alt="logo" style={{width:70,height:70}}/> */}
 
-                     
-                        {/* <img src={hey} alt="logo" style={{width:70,height:70}}/> */}
-                        
-
-                        <div className="headerIcon">
-                        <a href="" className="text">users </a>
-                        <img src={group} alt="users"/>
-                        </div>
-
-
-                        <div className="headerIcon">
-                        <a href="" className="text">order </a>
-                        <img src={order} alt="order"/>
-                        </div>
-
-
-                        <div className="headerIcon">
-                        <a href="" className="text">products </a>
-                        <img src={product} alt="products"/>
-                        </div>
-
-                        <div className="headerIcon notification">
-                        <a href="" className="text">more ... </a>
-                        </div>
-
-                        <div className="notificationProfile">
-                            <img src={notification} alt="notification" />
-                            <img src={girl} alt="profile" className="profile"/>
-                        </div>
-
-                    </div>
-
-                </p>
-
-
+            <div className="headerIcon">
+              <a href="#" className="text-style">
+                users{" "}
+              </a>
+              <img src={group} alt="users" />
             </div>
 
-      
-         );
-    }
+            <div className="headerIcon">
+              <a href="#" className="text-style">
+                order{" "}
+              </a>
+              <img src={order} alt="order" />
+            </div>
+
+            <div className="headerIcon">
+              <a href="#" className="text-style">
+                products{" "}
+              </a>
+              <img src={product} alt="products" />
+            </div>
+
+            <div className="headerIcon notification">
+              <a href="#" className="text-style">
+                more ...{" "}
+              </a>
+            </div>
+
+            <div className="notificationProfile">
+              <img src={notification} alt="notification" />
+              <img src={girl} alt="profile" className="profile" />
+            </div>
+          </div>
+        </p>
+      </div>
+    );
+  }
 }
- 
+
 export default Header;
