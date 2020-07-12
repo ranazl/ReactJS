aec98dd72729d4f36e19be3570bfa2c790e87ae1
Majid <majid@Majids-iMac.local>
2020-07-12 20:18:35 +0430

    Fix tofs

--- src/components/bookList.js
+++ src/components/bookList.js
@@ -1,18 +1,10 @@
-// import React from 'react';
-// import { ReactDOM } from 'react-dom';
+import React from "react";
+import { ReactDOM } from "react-dom";
 
-// function BookList(props) {
-//     const books = props.books;
-//     const listItems = books.map((book) =>
-//         <li>{book}</li>
-//     );
-//     return(
-//     <ul>{listItems}</ul>
-//     )
-// }
+function BookList(props) {
+  const { books } = props;
+  const listItems = books.map((book) => <li>{book}</li>);
+  return <ul>{listItems}</ul>;
+}
 
-// const books = ["rter","rtert","ryutyu","rqwr","rurj"];
-// ReactDOM.render(
-//     <BookList books={books} />,
-//     document.getElementById('root')
-// );
+export default BookList;
--- src/pages/content.js
+++ src/pages/content.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import PartOne from '../components/partOne';
-// import BookList from '../components/bookList';
+import React from "react";
+import PartOne from "../components/partOne";
+import BookList from "../components/bookList";
+
+const books = ["rter", "rtert", "ryutyu", "rqwr", "rurj"];
 
 function Content() {
   return (
     <div className="App">
-      <PartOne/>
-      {/* <BookList/> */}
+      <PartOne />
+      <BookList books={books} />
     </div>
   );
 }
