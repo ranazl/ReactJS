685c955fd1d62e06fa2b0f1bb3504302594d5d65
rana <zofaghari.r@dpi.ir>
2020-07-12 11:21:44 +0430

    continue header

--- src/components/header.js
+++ src/components/header.js
@@ -2,36 +2,48 @@ import React,{Component} from 'react';
 import '../css/header.css';
 import group from '../assets/group.png';
 import product from '../assets/product.png';
-import submit from '../assets/submit.png'
+import order from '../assets/order.png';
+import notification from '../assets/notification.png';
+import girl from '../assets/girl.jpg';
+// import hey from '../assets/hey.png';
 
 class Header extends Component {
     state = {  }
     render() { 
         return ( 
         
-            <div class="sideLinks">
+            <div >
                 <p>
                     <div id="headerLinks" class="row">
 
-                        <div class="headerIcon">
-                        <a href="" style={{color:"black",fontWeight:"bold"}}>users </a>
+                     
+                        {/* <img src={hey} alt="logo" style={{width:70,height:70}}/> */}
+                        
+
+                        <div className="headerIcon">
+                        <a href="" className="text">users </a>
                         <img src={group} alt="users"/>
                         </div>
 
 
-                        <div class="headerIcon">
-                        <a href="" style={{color:"black",fontWeight:"bold"}}>order </a>
-                        <img src={submit} alt="users"/>
+                        <div className="headerIcon">
+                        <a href="" className="text">order </a>
+                        <img src={order} alt="order"/>
                         </div>
 
 
-                        <div class="headerIcon">
-                        <a href="" style={{color:"black",fontWeight:"bold"}}>products </a>
-                        <img src={product} alt="users"/>
+                        <div className="headerIcon">
+                        <a href="" className="text">products </a>
+                        <img src={product} alt="products"/>
                         </div>
 
-                        <div class="headerIcon">
-                        <a href="" style={{color:"black",fontWeight:"bold"}}>more ... </a>
+                        <div className="headerIcon notification">
+                        <a href="" className="text">more ... </a>
+                        </div>
+
+                        <div className="notificationProfile">
+                            <img src={notification} alt="notification" />
+                            <img src={girl} alt="profile" className="profile"/>
                         </div>
 
                     </div>
